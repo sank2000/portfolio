c027b9819aac6633adbbfe13024c44bc81549bde
sank2000 <santhoshvelr@yahoo.com>
2020-12-14 18:50:52 +0530

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>san</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <h1>Welcome to protfolio</h1>
+    </>
+  );
+}
