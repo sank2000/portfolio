44b68e3259865152212e8b1cb6929f46f4d7edca
sank2000 <santhoshvelr@yahoo.com>
2020-12-20 12:19:34 +0530

    🎉 fav icon updated

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,8 +14,34 @@ export default function Index() {
   return (
     <>
       <Head>
-        <title>san</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>santhosh | personal portfolio</title>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/ico/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/ico/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/ico/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/ico/site.webmanifest" />
+        <link
+          rel="mask-icon"
+          href="/ico/safari-pinned-tab.svg"
+          color="#5bbad5"
+        />
+        <link rel="shortcut icon" href="/ico/favicon.ico" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="msapplication-config" content="/ico/browserconfig.xml" />
+        <meta name="theme-color" content="#ffffff" />
       </Head>
       <section className="section">
         <Particles />
