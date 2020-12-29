58ba9ce0ed205caf9486ff461be34a5cb4f51c72
sank2000 <santhoshvelr@yahoo.com>
2020-12-29 15:17:22 +0530

    about section added

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -28,7 +28,49 @@ export default function About({ page, setPage }: defaultProps) {
         <h3 className={classes.nav_head}>About</h3>
       </nav>
 
-      <main className={classes.main}>&nbsp;</main>
+      <main className={classes.main}>
+        <div className={classes.main__left}>
+          <div>
+            <h2>Hi</h2>
+            <img src="/hi.gif" alt="hi" />
+          </div>
+
+          <h3>
+            I am <span className={classes.name}>Santhosh</span> pursuing
+            Computer Science and Engineering at the University College of
+            Engineering, Trichy.
+          </h3>
+
+          <ul>
+            <li>
+              <img src="/icons/logo white.svg" alt="logo" />I am from Salem.
+            </li>
+            <li>
+              <img src="/icons/logo white.svg" alt="logo" />I am passionate
+              about coding & designing.
+            </li>
+            <li>
+              <img src="/icons/logo white.svg" alt="logo" />I am a good team
+              player.
+            </li>
+
+            <li>
+              <img src="/icons/logo white.svg" alt="logo" />I love to explore
+              new technologies and use them to solve real-life problems.
+            </li>
+            <li>
+              <img src="/icons/logo white.svg" alt="logo" />I spend my free time
+              scrolling through social media, hearing songs, and dance.
+            </li>
+          </ul>
+        </div>
+        <div className={classes.main__right}>
+          <div>
+            <img src="/me.jpg" alt="me" />
+          </div>
+          <a href="/">RESUME</a>
+        </div>
+      </main>
       <footer className={classes.footer}>
         <span
           className="icon-arrow"
