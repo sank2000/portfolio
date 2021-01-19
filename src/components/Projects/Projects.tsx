a635ccf478ed5b454d7999db6845d59063249a5d
sank2000 <santhoshvelr@yahoo.com>
2021-01-19 14:59:53 +0530

    project section improved 🐛

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -15,6 +15,7 @@ export default function Projects({ page, setPage }: defaultProps) {
   const [exitLeft, setExitLeft] = useState(false);
 
   const [active, setActive] = useState(0);
+  const [show, setShow] = useState(false);
   const intervalRef = useRef<any>(null);
 
   const setCurrent = () => {
@@ -27,7 +28,10 @@ export default function Projects({ page, setPage }: defaultProps) {
   };
 
   useEffect(() => {
-    setCurrent();
+    setTimeout(() => {
+      setShow(true);
+      setCurrent();
+    }, 1000);
 
     return () => {
       clearInterval(intervalRef.current);
@@ -78,7 +82,7 @@ export default function Projects({ page, setPage }: defaultProps) {
             onClick={() => handleClick(0)}
             style={{
               backgroundColor: active === 0 ? '#fff' : 'rgb(187, 187, 187)',
-              transform: active === 0 ? 'scale(1.3)' : 'scale(1)',
+              transform: active === 0 ? 'scale(1.2)' : 'scale(1)',
             }}
           >
             &nbsp;
@@ -87,7 +91,7 @@ export default function Projects({ page, setPage }: defaultProps) {
             onClick={() => handleClick(1)}
             style={{
               backgroundColor: active === 1 ? '#fff' : 'rgb(187, 187, 187)',
-              transform: active === 1 ? 'scale(1.3)' : 'scale(1)',
+              transform: active === 1 ? 'scale(1.2)' : 'scale(1)',
             }}
           >
             &nbsp;
@@ -96,7 +100,7 @@ export default function Projects({ page, setPage }: defaultProps) {
             onClick={() => handleClick(2)}
             style={{
               backgroundColor: active === 2 ? '#fff' : 'rgb(187, 187, 187)',
-              transform: active === 2 ? 'scale(1.3)' : 'scale(1)',
+              transform: active === 2 ? 'scale(1.2)' : 'scale(1)',
             }}
           >
             &nbsp;
@@ -105,67 +109,69 @@ export default function Projects({ page, setPage }: defaultProps) {
             onClick={() => handleClick(3)}
             style={{
               backgroundColor: active === 3 ? '#fff' : 'rgb(187, 187, 187)',
-              transform: active === 3 ? 'scale(1.3)' : 'scale(1)',
+              transform: active === 3 ? 'scale(1.2)' : 'scale(1)',
             }}
           >
             &nbsp;
           </div>
         </div>
-        <div
-          className={classes.card_container}
-          onMouseEnter={() => clearInterval(intervalRef.current)}
-          onMouseLeave={() => setCurrent()}
-          onTouchStart={() => clearInterval(intervalRef.current)}
-          onTouchEnd={() => setCurrent()}
-        >
-          <AnimatePresence exitBeforeEnter>
-            {active === 0 && (
-              <Card
-                key="nviktous"
-                name="nviktous"
-                image="/nviktous.png"
-                description="It is completely packed E-Commerce site with options like OAuth, OTP etc. which is build using MERN stack.It also contains admin panel where the seller
+        {show && (
+          <div
+            className={classes.card_container}
+            onMouseEnter={() => clearInterval(intervalRef.current)}
+            onMouseLeave={() => setCurrent()}
+            onTouchStart={() => clearInterval(intervalRef.current)}
+            onTouchEnd={() => setCurrent()}
+          >
+            <AnimatePresence exitBeforeEnter>
+              {active === 0 && (
+                <Card
+                  key="nviktous"
+                  name="nviktous"
+                  image="/nviktous.png"
+                  description="It is completely packed E-Commerce site with options like OAuth, OTP etc. which is build using MERN stack.It also contains admin panel where the seller
                         can handle the sales process"
-                stack={[react, nodejs, mongodb, firebase]}
-                link="https://nviktous.herokuapp.com/"
-                github={null}
-              />
-            )}
-            {active === 1 && (
-              <Card
-                key="college mate"
-                name="college mate"
-                image="/collegemate.png"
-                description="A MERN stack Progressive Web App (PWA) built for students and teaching faculties to easily share materials, homework, assignments, and notes."
-                stack={[react, nodejs, mongodb, firebase, pwa]}
-                link="https://collegemate.herokuapp.com/"
-                github={null}
-              />
-            )}
-            {active === 2 && (
-              <Card
-                key="biller"
-                name="biller"
-                image="/biller.png"
-                description=" It is MERN Stack application for online bill payment with payment methods like PayPal and Stripe."
-                stack={[react, nodejs, mongodb]}
-                link="https://san-biller.herokuapp.com/"
-                github="https://github.com/sank2000/Biller"
-              />
-            )}
-            {active === 3 && (
-              <Card
-                key="aubit"
-                name="aubit"
-                image="/aubit.png"
-                description="The unofficial website of University College of Engineering, BIT Campus, Tiruchirappalli. The website is still under development and will be approved upon completion to be the official website. Built with React."
-                stack={[react, nodejs]}
-                link="https://aubit.netlify.app/"
-                github={null}
-              />
-            )}
-          </AnimatePresence>
-        </div>
+                  stack={[react, nodejs, mongodb, firebase]}
+                  link="https://nviktous.herokuapp.com/"
+                  github={null}
+                />
+              )}
+              {active === 1 && (
+                <Card
+                  key="college mate"
+                  name="college mate"
+                  image="/collegemate.png"
+                  description="A MERN stack Progressive Web App (PWA) built for students and teaching faculties to easily share materials, homework, assignments, and notes."
+                  stack={[react, nodejs, mongodb, firebase, pwa]}
+                  link="https://collegemate.herokuapp.com/"
+                  github={null}
+                />
+              )}
+              {active === 2 && (
+                <Card
+                  key="biller"
+                  name="biller"
+                  image="/biller.png"
+                  description=" It is MERN Stack application for online bill payment with payment methods like PayPal and Stripe."
+                  stack={[react, nodejs, mongodb]}
+                  link="https://san-biller.herokuapp.com/"
+                  github="https://github.com/sank2000/Biller"
+                />
+              )}
+              {active === 3 && (
+                <Card
+                  key="aubit"
+                  name="aubit"
+                  image="/aubit.png"
+                  description="The unofficial website of University College of Engineering, BIT Campus, Tiruchirappalli. The website is still under development and will be approved upon completion to be the official website. Built with React."
+                  stack={[react, nodejs]}
+                  link="https://aubit.netlify.app/"
+                  github={null}
+                />
+              )}
+            </AnimatePresence>
+          </div>
+        )}
       </main>
     </motion.div>
   );
