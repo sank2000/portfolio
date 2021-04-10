cc69d9de2062724a38aea18ee099dec331edf797
sank2000 <santhoshvelr@yahoo.com>
2021-04-10 11:16:45 +0530

    updated meta tags 🎉

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -49,6 +49,20 @@ export default function Index() {
         <meta name="msapplication-TileColor" content="#da532c" />
         <meta name="msapplication-config" content="/ico/browserconfig.xml" />
         <meta name="theme-color" content="#252a34" />
+
+        <meta name="title" content="Santhosh | personal portfolio" />
+        <meta
+          name="description"
+          content="Santhosh is an aspiring UI/ UX designer & Full stack web developer, interested in designing  and building beautiful and functional websites and web applications."
+        />
+        <meta
+          name="keywords"
+          content="san, sank2000,santhosh, portfolio, personal website, santhosh k"
+        />
+        <meta name="robots" content="index, follow" />
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="language" content="English" />
+        <meta name="author" content="Santhosh K" />
       </Head>
       <>
         {loader && <Loader />}
