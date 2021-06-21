8926d71e37cfa59770252b999beba60465e4ed3b
sank2000 <santhoshvelr@yahoo.com>
2021-06-21 13:25:42 +0530

    added arroe key events  ✔

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -28,12 +28,63 @@ export default function Index() {
   const FIRST_PAGE = 1;
   const LAST_PAGE = 5;
 
+  function handleLeft() {
+    if (page.no !== FIRST_PAGE && page.no !== LAST_PAGE) {
+      return setExitLeft({
+        status: true,
+      });
+    }
+    setPage((old) => {
+      if (old.no === FIRST_PAGE) {
+        return old;
+      }
+      return {
+        forward: false,
+        no: old.no - 1,
+      };
+    });
+  }
+
+  function handleRight() {
+    if (page.no !== FIRST_PAGE && page.no !== LAST_PAGE) {
+      return setExitLeft({
+        status: false,
+      });
+    }
+    setPage((old) => {
+      if (old.no === LAST_PAGE) {
+        return old;
+      }
+      return {
+        forward: true,
+        no: old.no + 1,
+      };
+    });
+  }
+
   useEffect(() => {
     document.documentElement.lang = 'en-us';
+
     setTimeout(() => {
       setLoader(false);
     }, 3500);
   }, []);
+
+  useEffect(() => {
+    function handler(event) {
+      const key = event.key; // "ArrowRight", "ArrowLeft"
+      if (key === 'ArrowRight') {
+        handleRight();
+      } else if (key === 'ArrowLeft') {
+        handleLeft();
+      }
+    }
+    window.addEventListener('keydown', handler);
+
+    return () => {
+      window.removeEventListener('keydown', handler);
+    };
+  }, [page]);
 
   useEffect(() => {
     if (initialRef.current) {
@@ -47,38 +98,8 @@ export default function Index() {
   }, [exitLeft]);
 
   const handlers = useSwipeable({
-    onSwipedLeft: (eventData) => {
-      if (page.no !== FIRST_PAGE && page.no !== LAST_PAGE) {
-        return setExitLeft({
-          status: true,
-        });
-      }
-      setPage((old) => {
-        if (old.no === FIRST_PAGE) {
-          return old;
-        }
-        return {
-          forward: false,
-          no: old.no - 1,
-        };
-      });
-    },
-    onSwipedRight: (eventData) => {
-      if (page.no !== FIRST_PAGE && page.no !== LAST_PAGE) {
-        return setExitLeft({
-          status: false,
-        });
-      }
-      setPage((old) => {
-        if (old.no === LAST_PAGE) {
-          return old;
-        }
-        return {
-          forward: true,
-          no: old.no + 1,
-        };
-      });
-    },
+    onSwipedLeft: handleLeft,
+    onSwipedRight: handleRight,
   });
 
   return (
