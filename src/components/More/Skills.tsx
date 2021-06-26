ae8d263fbe90c29c1a03471bc8306b912dce2cfc
sank2000 <santhoshvelr@yahoo.com>
2021-06-26 11:56:17 +0530

    added Skill component ✔

--- src/components/More/Skills.tsx
+++ src/components/More/Skills.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { ReactChildren, ReactChild } from 'react';
 
 import classes from './style.module.scss';
 
@@ -6,21 +6,21 @@ export default function Skills() {
   return (
     <>
       <div className={classes.icon_container}>
-        <span className="icon-html5"></span>
-        <span className="icon-css3"></span>
-        <span className="icon-sass"></span>
-        <span className="icon-styled-components"></span>
-        <span className="icon-javascript"></span>
-        <span className="icon-typescript"></span>
-        <span className="icon-react"></span>
-        <span className="icon-next-dot-js"></span>
-        <span className="icon-redux"></span>
-        <span className="icon-figma"></span>
-        <span className="icon-node-dot-js"></span>
-        <span className="icon-express"></span>
-        <span className="icon-graphql"></span>
-        <span className="icon-apollo"></span>
-        <span className="icon-sequelize">
+        <Skill icon="icon-html5"></Skill>
+        <Skill icon="icon-css3"></Skill>
+        <Skill icon="icon-sass"></Skill>
+        <Skill icon="icon-styled-components"></Skill>
+        <Skill icon="icon-javascript"></Skill>
+        <Skill icon="icon-typescript"></Skill>
+        <Skill icon="icon-react"></Skill>
+        <Skill icon="icon-next-dot-js"></Skill>
+        <Skill icon="icon-redux"></Skill>
+        <Skill icon="icon-figma"></Skill>
+        <Skill icon="icon-node-dot-js"></Skill>
+        <Skill icon="icon-express"></Skill>
+        <Skill icon="icon-graphql"></Skill>
+        <Skill icon="icon-apollo"></Skill>
+        <Skill icon="icon-sequelize">
           <span className="path1"></span>
           <span className="path2"></span>
           <span className="path3"></span>
@@ -48,22 +48,39 @@ export default function Skills() {
           <span className="path25"></span>
           <span className="path26"></span>
           <span className="path27"></span>
-        </span>
-        <span className="icon-jest"></span>
-        <span className="icon-docker"></span>
+        </Skill>
+        <Skill icon="icon-jest"></Skill>
+        <Skill icon="icon-docker"></Skill>
       </div>
       <div className={classes.icon_container}>
-        <span className="icon-mongodb"></span>
-        <span className="icon-mysql"></span>
-        <span className="icon-postgresql"></span>
-        <span className="icon-firebase"></span>
+        <Skill icon="icon-mongodb"></Skill>
+        <Skill icon="icon-mysql"></Skill>
+        <Skill icon="icon-postgresql"></Skill>
+        <Skill icon="icon-firebase"></Skill>
       </div>
       <div className={classes.icon_container}>
-        <span className="icon-java"></span>
-        <span className="icon-python"></span>
-        <span className="icon-c"></span>
-        <span className="icon-cplusplus"></span>
+        <Skill icon="icon-java"></Skill>
+        <Skill icon="icon-python"></Skill>
+        <Skill icon="icon-c"></Skill>
+        <Skill icon="icon-cplusplus"></Skill>
       </div>
     </>
+  );
+}
+
+function Skill({
+  title,
+  icon,
+  children,
+}: {
+  title?: string;
+  icon: string;
+  children?: React.ReactNode | ReactChild | ReactChildren;
+}) {
+  return (
+    <span className={icon}>
+      {children && children}
+      {title && <span>{title}</span>}
+    </span>
   );
 }
