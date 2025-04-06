79020e455da4342b2a27c202dddcf77998d49c01
santhosh.velr <santhosh.velr@zohocorp.com>
2025-04-06 15:48:06 +0530

    about section update

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -8,14 +8,6 @@ import { containerVariants } from '@/constants';
 import { withAdditionalProps } from 'types';
 
 export default function About({ page, exitLeft, setExitLeft }: withAdditionalProps) {
-	const points = [
-		'I am from the so-called "Mango City", Salem.',
-		'I am passionate about coding & designing, inquisitive in exploring new technologies to solve real-life problems',
-		'I love working with people as a team and I am a good team player too',
-		'Ready to give a shot to everything, I wanted in life.',
-		'I spend my free time scrolling social media, listening to songs, and dancing.'
-	];
-
 	return (
 		<motion.div
 			key='about'
@@ -29,8 +21,9 @@ export default function About({ page, exitLeft, setExitLeft }: withAdditionalPro
 			<main className={classes.main}>
 				<div className={classes.main__left}>
 					<div>
-						<h2>Hi</h2>
+						<h2>Hello Mate</h2>
 						<img src='/images/hi.gif' alt='hi' />
+						<h2>,</h2>
 					</div>
 
 					<h3>
@@ -38,28 +31,17 @@ export default function About({ page, exitLeft, setExitLeft }: withAdditionalPro
 						<span className={classes.short_name}>San, </span> Software Developer at Zoho Corp.
 					</h3>
 
-					<ul>
-						{points.map((point, ind) => {
-							return (
-								<li key={ind}>
-									<img src='/images/logo/logo white.svg' alt='logo' />
-									{point}
-								</li>
-							);
-						})}
-					</ul>
+					<h4>
+						With a solid background in full-stack development, I specialize in building scalable and
+						high-performance applications. I am passionate about developing innovative solutions
+						that optimize system performance and drive efficiency. I always look forward to new
+						opportunities to learn and grow.
+					</h4>
 				</div>
 				<div className={classes.main__right}>
 					<div>
-						<img src='/images/professional san darkbg.png' alt='me' />
+						<img src='/images/personal_image.png' alt='me' />
 					</div>
-					<a
-						target='_blank'
-						rel='noreferrer'
-						href='https://drive.google.com/file/d/1yJKO8J9rYKVmrRLTG3kuaAkv64b3mpLK/view?usp=sharing'
-					>
-						CV
-					</a>
 				</div>
 			</main>
 		</motion.div>
