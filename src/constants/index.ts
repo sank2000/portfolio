f09d568d06be42de85785a170bc4afc195540e3d
sank2000 <santhoshvelr@gmail.com>
2025-04-06 16:10:23 +0530

    build issue fix

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,5 @@
 import cardVariants from './Variants/cardVariants';
 import containerVariants from './Variants/containerVariants';
 import experience from './experience';
-import { showcase } from './projects';
 
-export { containerVariants, cardVariants, experience, showcase };
+export { containerVariants, cardVariants, experience };
